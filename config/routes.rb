ef8a62f0d8e22ac5908ea96b0fcbe5fdc9bab8e5
Shabab Ali <shababsaifi@gmail.com>
2020-11-07 01:18:26 +0530

    added token based authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+    end
+  end
+  post 'login', to: 'authentication#login'
+  get 'auto_login', to: 'authentication#auto_login'
+  get 'user_is_authed', to: 'authentication#user_is_authed'
 end
