d8ee9247310e32c637403c4af37dadc7955706f1
Shabab Ali <shababsaifi@gmail.com>
2020-11-08 14:57:49 +0530

    added tests for authentication requests

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe 'Users', type: :request do
   it 'creates a new user with status code 201' do
     post '/api/v1/users', params:
       { name: 'John Doe',
-        email: 'abcdef@mail.com',
+        email: 'abcdefg@mail.com',
         password: '1234555',
         password_confirmation: '1234555' }
-
+    expect(response.content_type).to eq('application/json')
     expect(response).to have_http_status(:created)
   end
 end
